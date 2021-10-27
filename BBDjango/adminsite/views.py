6c78a3dfb7c2d2da62abd9a8e3a3b7f4b14016fa
Nigelle <91709880+Nigelle@users.noreply.github.com>
2021-10-27 20:20:15 +0800

    Update views.py

--- BBDjango/adminsite/views.py
+++ BBDjango/adminsite/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+
 
 # Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+
+def adminaccess(request):
+  return render(request,"adminaccess.html")
+
+def admindonors(request):
+  return render(request,"admindonors.html")
+
+def adminlogin(request):
+  return render(request,"adminlogin.html")
+
+def adminpatient(request):
+  return render(request,"adminpatient.html")
