11b1300a199ad02b643aafe2047df354c1fe5636
michaelrilan <micorilan1999@gmail.com>
2021-10-27 18:50:22 +0800

    add codes urls

--- BBDjango/BBDjango/urls.py
+++ BBDjango/BBDjango/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include    
 
 urlpatterns = [
-    path('mainbb/', include('mainbb.urls')),
     path('admin/', admin.site.urls),
+    path('mainbb/', include('mainbb.urls')),
+    
 ]
--- BBDjango/mainbb/views.py
+++ BBDjango/mainbb/views.py
@@ -5,4 +5,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  return HttpResponse("wassup")
+  return render(request,"index.html")
+
+def admin(request):
+  return render(request,"adminlogin.html")
