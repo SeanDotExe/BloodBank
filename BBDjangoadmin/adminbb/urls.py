69e2f73e77114db554966acbf9926562fbb19182
michaelrilan <micorilan1999@gmail.com>
2021-11-19 11:12:45 +0800

    Some changes in the design

--- BBDjangoadmin/adminbb/urls.py
+++ BBDjangoadmin/adminbb/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
   path('', views.defadminlogin, name='adminlogin'),
-
   path('adminlogin.html', views.defadminlogin2, name='adminlogin'),
   path('adminaccess.html', views.defadminaccess, name='adminaccess'),
   path('admindonors.html', views.defadmindonors, name='admindonors'),
