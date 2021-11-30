c241c7ba8564600148e2c7f6127fa7bb49934e4c
Michael Angelo Rilan <83159662+michaelrilan@users.noreply.github.com>
2021-11-30 18:09:24 +0800

    Update forms.py
    I added donate and need functions

--- BBDjango/mainbb/forms.py
+++ BBDjango/mainbb/forms.py
@@ -1,7 +1,16 @@
 from django import forms
-from .models import reg_donor_patient
+from .models import reg_donor_patient,add_reqdonate, add_reqblood
 
 class registration_form(forms.ModelForm):
     class Meta:
         model = reg_donor_patient
+        fields = '__all__'
+class donate(forms.ModelForm):
+    class Meta:
+        model = add_reqdonate
+        fields = '__all__'
+
+class need(forms.ModelForm):
+    class Meta:
+        model = add_reqblood
         fields = '__all__'
