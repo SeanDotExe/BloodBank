0ac3fd0de1ecc0a2c446316b7f8d221894f3bbab
michaelrilan <micorilan1999@gmail.com>
2022-01-28 09:16:33 +0800

    some finishing touches

--- BBDjango/adminbb/views.py
+++ BBDjango/adminbb/views.py
@@ -218,53 +218,58 @@ def defdonorapproved_screening(request,id):
 
 @login_required(login_url='adminlogin')
 def defdonor_screening(request,id):
-  if request.user.is_authenticated and request.user.is_staff:
-    donor_info = add_reqdonate.objects.get(id=id)
-    apos_blood_count = blood_counter.objects.values_list('Apos', flat=True).get(pk=1)
-    aneg_blood_count = blood_counter.objects.values_list('Aneg', flat=True).get(pk=1)
-    bpos_blood_count = blood_counter.objects.values_list('Bpos', flat=True).get(pk=1)
-    bneg_blood_count = blood_counter.objects.values_list('Bneg', flat=True).get(pk=1)
-    abpos_blood_count = blood_counter.objects.values_list('ABpos', flat=True).get(pk=1)
-    abneg_blood_count = blood_counter.objects.values_list('ABneg', flat=True).get(pk=1)
-    opos_blood_count = blood_counter.objects.values_list('Opos', flat=True).get(pk=1)
-    oneg_blood_count = blood_counter.objects.values_list('Oneg', flat=True).get(pk=1)
+  donor_info = add_reqdonate.objects.get(id=id)
+  apos_blood_count = blood_counter.objects.values_list('Apos', flat=True).get(pk=1)
+  aneg_blood_count = blood_counter.objects.values_list('Aneg', flat=True).get(pk=1)
+  bpos_blood_count = blood_counter.objects.values_list('Bpos', flat=True).get(pk=1)
+  bneg_blood_count = blood_counter.objects.values_list('Bneg', flat=True).get(pk=1)
+  abpos_blood_count = blood_counter.objects.values_list('ABpos', flat=True).get(pk=1)
+  abneg_blood_count = blood_counter.objects.values_list('ABneg', flat=True).get(pk=1)
+  opos_blood_count = blood_counter.objects.values_list('Opos', flat=True).get(pk=1)
+  oneg_blood_count = blood_counter.objects.values_list('Oneg', flat=True).get(pk=1)
 
-    donorform = donor_screening_form(request.POST or None, instance=donor_info)
+  donorform = donor_screening_form(request.POST or None, instance=donor_info)
     
-    context = {
-          "donorform": donorform,
-          "donor": donor_info
-          }
+  context = {
+        "donorform": donorform,
+        "donor": donor_info
+        }
       
 
-    if donorform.is_valid():
+  if donorform.is_valid():
             #blood_counter.objects.filter(pk=1).update(Apos= apos_blood_count-patient_info.unit)
-        donorform.save()
-        getstatus = add_reqdonate.objects.values_list('status', flat=True).get(pk=id)
-        getbloodtype = add_reqdonate.objects.values_list('bloodtype', flat=True).get(pk=id)
-        if getstatus == "Approved":
-          if getbloodtype == "A+":
-            blood_counter.objects.filter(pk=1).update(Apos= apos_blood_count+donor_info.unit)
-          elif getbloodtype == "A-":
-            blood_counter.objects.filter(pk=1).update(Aneg= aneg_blood_count+donor_info.unit)
-          elif getbloodtype == "B+":
-            blood_counter.objects.filter(pk=1).update(Bpos= bpos_blood_count+donor_info.unit)
-          elif getbloodtype == "B-":
-            blood_counter.objects.filter(pk=1).update(Bneg= bneg_blood_count+donor_info.unit)
-          elif getbloodtype == "AB+":
-            blood_counter.objects.filter(pk=1).update(ABpos= abpos_blood_count+donor_info.unit)
-          elif getbloodtype == "AB-":
-            blood_counter.objects.filter(pk=1).update(ABneg= abneg_blood_count+donor_info.unit)
-          elif getbloodtype == "O+":
-            blood_counter.objects.filter(pk=1).update(Opos= opos_blood_count+donor_info.unit)
-          elif getbloodtype == "O-":
-            blood_counter.objects.filter(pk=1).update(Oneg= oneg_blood_count+donor_info.unit)
+    donorform.save()
+    getstatus = add_reqdonate.objects.values_list('status', flat=True).get(pk=id)
+    getbloodtype = add_reqdonate.objects.values_list('bloodtype', flat=True).get(pk=id)
+    if getstatus == "Approved":
+      if getbloodtype == "A+":
+        blood_counter.objects.filter(pk=1).update(Apos= apos_blood_count+1)
+        return redirect("admindonors")
+      elif getbloodtype == "A-":
+        blood_counter.objects.filter(pk=1).update(Aneg= aneg_blood_count+1)
+        return redirect("admindonors")
+      elif getbloodtype == "B+":
+        blood_counter.objects.filter(pk=1).update(Bpos= bpos_blood_count+1)
+        return redirect("admindonors")
+      elif getbloodtype == "B-":
+        blood_counter.objects.filter(pk=1).update(Bneg= bneg_blood_count+1)
+        return redirect("admindonors")
+      elif getbloodtype == "AB+":
+        blood_counter.objects.filter(pk=1).update(ABpos= abpos_blood_count+1)
+        return redirect("admindonors")
+      elif getbloodtype == "AB-":
+        blood_counter.objects.filter(pk=1).update(ABneg= abneg_blood_count+1)
+        return redirect("admindonors")
+      elif getbloodtype == "O+":
+        blood_counter.objects.filter(pk=1).update(Opos= opos_blood_count+1)
+        return redirect("admindonors")
+      elif getbloodtype == "O-":
+        blood_counter.objects.filter(pk=1).update(Oneg= oneg_blood_count+1)
+        return redirect("admindonors")
 
           
 
-        return redirect("admindonors")
-  else:
-    return redirect('adminlogin')
+      
 
   return render(request,"donor_screening.html",context)
 
